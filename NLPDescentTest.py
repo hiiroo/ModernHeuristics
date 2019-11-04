3bb95dcb407b6e282338fb0a4738d671cbf48c43
Ali Ceylan <alimrtcyln@gmail.com>
2019-11-04 12:13:45 +0300

    Minor fixes and Major addons
    Licence headers added for TSP* files.
    NLP2D, 3D and RealNumberGenome added.

--- NLPDescentTest.py
+++ NLPDescentTest.py
@@ -23,15 +23,3 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from TSPInstance import TSPInstance
-from TSPDescent import TSPDescent
-from TSPA import TSPA
-from TSPTS import TSPTS
-
-
-if __name__ == "__main__":
-    instance = TSPInstance("berlin52.tsp")
-
-    TSPDescent.run(100, instance)
-    TSPA.run(1000, instance)
-    TSPTS.run(30000, instance, 400)
